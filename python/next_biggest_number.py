e4f56219f555644b11dd45db32f2c94303851c2b
areeratk <areeratk@gmail.com>
2021-09-17 10:08:32 -0500

    update2

--- python/next_biggest_number.py
+++ python/next_biggest_number.py
@@ -13,24 +13,25 @@ def next_biggest_number(num):
     n = len(str(num))
     number = [int(x) for x in str(num)]
 
-    #Start from the farthest right digit, find the first digit that is smaller than the digit next to it     
+    #Start from the farthest right digit of the array, find the smaller first digit next to it     
     for i in range(n-1,0,-1):
          if number[i] > number[i-1]:
              break
               
-    # If such smaller digit is non-existent, return -1
+    #If find no such next smaller digit, the numbers must be in a descending order
+    #Return -1 when the next biggest number cannot be created
     if i == 1 and number[i] <= number[i-1]:
         print ("-1")
         return -1
           
-    # Find the smallest digit on the right side of (i-1)'th digit that is greater than number[i-1]
+    #Find the smallest digit on the right side of (i-1)'th digit that is greater than number[i-1]
     x = number[i-1]
     smallest = i
     for j in range(i+1,n):
         if number[j] > x and number[j] < number[smallest]:
             smallest = j
           
-    # Swapping the above found smallest digit with (i-1)'th
+    #Swapping the above found smallest digit with (i-1)'th
     number[smallest],number[i-1] = number[i-1], number[smallest]
       
     # x is the final number, in integer datatype
